bbaa6eb3ac2693f8620ec36a15e1693e76155bf6
Dropio12 <anass.elhallaoui@gmail.com>
2022-07-14 20:08:56 -0400

    Test to create an interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,18 @@
 package com.company;
+import java.awt.*;
 
-import java.util.Arrays;
 
-public class Main {
+class Fenêtre {
+    public final static int LARGEUR = 800;
+    public final static int HAUTEUR = 600;
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(rotate(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3)));
+        Frame F = new Frame();
+        F.setTitle("Fenêtre");
+        F.setSize(LARGEUR, HAUTEUR);
+        F.setBackground(Color.darkGray);
+        F.setVisible(true);
     }
-
-
-    public static int[] rotate(int[] nums, int k) {
-        int[] result = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            result[(i + k) % nums.length] = nums[i];
-        }
-        return result;
-    }
-
 }
+
+
