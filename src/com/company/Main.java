4916e36bf947919e6a34454369ac997717be423c
Dropio12 <anass.elhallaoui@gmail.com>
2022-07-27 19:24:47 -0400

    Well thats the 3 things application! I dont know why it doesnt work on the other commit, but here it works.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,64 @@
 package com.company;
+import javax.swing.*;
 import java.awt.*;
 
 
 class Fenêtre {
-    public final static int LARGEUR = 800;
-    public final static int HAUTEUR = 600;
+    public final static int LARGEUR = 500;
+    public final static int HAUTEUR = 150;
 
     public static void main(String[] args) {
         Frame F = new Frame();
         F.setTitle("Fenêtre");
         F.setSize(LARGEUR, HAUTEUR);
-        F.setBackground(Color.darkGray);
+        F.setBackground(Color.PINK);
         F.setVisible(true);
+        F.setTitle("3 Things To Do Today!"); ;
+
+        Checkbox b= new Checkbox("Complete 4 pages of math exercises");
+        b.setFont(new Font("Arial", Font.BOLD, 20));
+        F.add(b);
+
+        Checkbox c= new Checkbox("Go gym!");
+        c.setFont(new Font("Arial", Font.BOLD, 20));
+        F.add(c);
+
+        Checkbox d= new Checkbox("Read 10 pages of the productivity project");
+        d.setFont(new Font("Arial", Font.BOLD, 20));
+        F.add(d);
+
+        Checkbox e= new Checkbox("Read 10 pages of the productivity project");
+        F.add(e);
+
+        b.setBounds(10, 30, 400, 30);
+        c.setBounds(10, 60, 400, 30);
+        d.setBounds(10, 90, 800, 30);
+
+        F.setVisible(true);
+        F.setLayout(null);
+        F.setAlwaysOnTop(true);
+        F.setResizable(false);
+        if(b.getState() && c.getState() && d.getState() && e.getState()){
+            F.setTitle("You are done!");
+        }
+
+        while(true){
+            try{
+                Thread.sleep(3600000);
+            int r = (int)(Math.random()*256);
+            int g = (int)(Math.random()*256);
+            int k = (int)(Math.random()*256);
+            F.setBackground(new Color(r,g,k));
+
+            }
+            catch(InterruptedException p){
+                System.out.println("Interrupted");
+            }
+        }
     }
-}
+
+        }
+
+
 
 
